76815dc369d77d98b6a2045eea81893d35ebd072
Paul Bob <paul.ionut.bob@gmail.com>
2025-07-30 11:43:57 +0300

    add tw4

--- lib/avo/rhino_field/engine.rb
+++ lib/avo/rhino_field/engine.rb
@@ -5,14 +5,22 @@ module Avo
     class Engine < Rails::Engine
       isolate_namespace Avo::RhinoField
 
+      initializer "avo.assets" do |app|
+        if app.config.respond_to?(:assets)
+          if defined?(Sprockets)
+            app.config.assets.precompile += %w[avo-rhino_field.js]
+          end
+        end
+      end
+
       initializer "avo-rhino_field.init" do |app|
         ActiveSupport.on_load(:avo_boot) do
           Avo.plugin_manager.register :rhino
 
           Avo.plugin_manager.register_field :rhino, Avo::RhinoField::Fields::RhinoField
 
-          Avo.asset_manager.add_stylesheet "avo-rhino_field"
-          Avo.asset_manager.add_javascript "avo-rhino_field"
+          Avo.asset_manager.add_stylesheet "avo-rhino_field/application"
+          Avo.asset_manager.add_javascript "avo-rhino_field/application"
         end
 
         app.config.assets.paths << root.join("app", "assets", "svgs", "avo")
