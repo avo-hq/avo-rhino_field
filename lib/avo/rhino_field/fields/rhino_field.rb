fe091043563930bd219e89df8a34d1f46a806e44
Adrian <adrian@adrianthedev.com>
2026-08-14 23:26:43 +0300

    Declare the resizable editor viewport for Avo >= 4.2

--- lib/avo/rhino_field/fields/rhino_field.rb
+++ lib/avo/rhino_field/fields/rhino_field.rb
@@ -2,6 +2,9 @@ module Avo
   module RhinoField
     module Fields
       class RhinoField < Avo::Fields::BaseField
+        # Avo >= 4.2 lets the editor viewport be resized with a persisted height.
+        resizable_editor target: "avo-rhino-editor > .trix-content[slot='editor']" if respond_to?(:resizable_editor)
+
         attr_reader :always_show
 
         def initialize(id, **args, &block)
